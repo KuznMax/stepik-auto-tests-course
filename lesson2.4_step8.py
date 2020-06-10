bbec5ac542699408aab2202ab477a93f045b1f89
Max Kuzn <kuzn.max@gmail.com>
2020-06-10 05:21:10 +0500

    add files *.py

---
+++ lesson2.4_step8.py
@@ -0,0 +1,17 @@
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium import webdriver
+from math import log, sin
+
+browser = webdriver.Chrome()
+browser.get("http://suninjuly.github.io/explicit_wait2.html")
+
+WebDriverWait(browser, 15).until(EC.text_to_be_present_in_element((By.ID, "price"), "$100"))
+browser.find_element_by_id("book").click()
+
+browser.find_element_by_id("answer").send_keys(
+    str(log(abs(12*sin(int(browser.find_element_by_id("input_value").text)))))
+    )
+
+browser.find_element_by_id("solve").click()
